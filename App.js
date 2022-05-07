7eba5f729088a99dd58ea14f2df84bde33912bcd
shivangirajput123 <rajputshivangiii987@gmail.com>
2022-05-07 08:12:18 +0530

    score card changed

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-
 const quizDB = [
     {
-        question: "Q1: What is the full form of HTML?",
+        question: "Q: What is the full form of HTML?",
         a: "Hello To My Language",
         b: "Hey Text Markup Language",
         c: "HyperText Markup Language",
@@ -9,7 +8,7 @@ const quizDB = [
         ans: "ans4"
     },
     {
-        question: "Which of the following tag is used for inserting the largest heading in HTML?",
+        question: "Q:Which of the following tag is used for inserting the largest heading in HTML?",
         a: "head",
         b: "<h1>",
         c: "<h6>",
@@ -17,7 +16,7 @@ const quizDB = [
         ans: "ans2"
     },
     {
-        question: "What is DOM in HTML?",
+        question: "Q:What is DOM in HTML?",
         a: "Language dependent application programming",
         b: "Hierarchy of objects in ASP.NET",
         c: "Application programming interface",
@@ -25,7 +24,7 @@ const quizDB = [
         ans: "ans4"
     },
     {
-        question: "In which part of the HTML metadata is contained?",
+        question: "Q:In which part of the HTML metadata is contained?",
         a: "head tag",
         b: "title tag",
         c: "html tag",
@@ -33,7 +32,7 @@ const quizDB = [
         ans: "ans1"
     },
     {
-        question: "Which element is used to get highlighted text in HTML5?",
+        question: "Q:Which element is used to get highlighted text in HTML5?",
         a: "<u>",
         b: "<mark>",
         c: "<highlight>",
@@ -41,68 +40,54 @@ const quizDB = [
         ans: "ans2"
     },
 ];
-
 const question = document.querySelector('.question');
 const option1 = document.querySelector('#option1');
 const option2 = document.querySelector('#option2');
 const option3 = document.querySelector('#option3');
 const option4 = document.querySelector('#option4');
 const submit = document.querySelector('#submit');
-
 const answers = document.querySelectorAll('.answer');
-
-const showScore = document.getElementById("showScore");
-
+const showScore = document.querySelector('#showScore');
 let questionCount = 0;
 let score =0;
 const loadQuestion = () => {
     const questionList = quizDB[questionCount];
-
     question.innerText = questionList.question;
-
     option1.innerText = questionList.a;
     option2.innerText = questionList.b;
     option3.innerText = questionList.c;
     option4.innerText = questionList.d;
 }
-
 loadQuestion();
-
 const getCheckAnswer = () => {
     let answer;
-
     answers.forEach((curAnsELem) => {
         if(curAnsELem.checked){
             answer = curAnsELem.id;
         }
-    
     });
     return answer;
 };
-
 const deselectAll = () => {
     answers.forEach((curAnsELem) => curAnsELem.checked = false );
 }
-
 submit.addEventListener('click', () => {
     const checkedAnswer = getCheckAnswer();
     console.log(checkedAnswer);
-
     if(checkedAnswer === quizDB[questionCount].ans){
         score++;
     };
-
     questionCount++;
-
     deselectAll();
-
-
     if(questionCount < quizDB.length){
         loadQuestion();
     }
     else{
-        showScore.innerHTML = "your final score is "  + score;
-
-        showScore.classList.remove('scoreArea');
+        showScore.innerHTML = `
+        <h3>You scored ${score}/${quizDB.length}'emoji✌ </h3>
+        <button class="btn" onclick="location.reload()">Playagain</button>
+        `;
+        showScore.classList.remove('.scorearea');
+        
     }
 });
